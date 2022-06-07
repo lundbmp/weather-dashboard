e49d1a1723f2459ec56786c464c13a4ae6f0549b
lundbmp <m.lundberg008@gmail.com>
2022-06-06 20:54:54 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,37 @@
+// will house previously searched cities
 let historyArray;
 
+// commonly used elements for populating the HTML
 let formEl = document.querySelector("#form");
 let historyContainerEl = document.querySelector("#history");
 let currentWeatherEl = document.querySelector("#current-weather");
 let forecastEl = document.querySelector("#forecast");
 
+// when page loads, populate history array and history buttons
 window.onload = function() {
     loadHistoryArray();
     addHistoryButtons();
 };
 
+/*
+####### Event Handelers #######
+*/
+// function for searching for a new city
 function formSubmitHandeler(event) {
+    // getting the city name from the form and setting it into the geo location api url
     let cityName = document.querySelector(".form-control").value.trim().toUpperCase();
     let apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=7347a58ac895179cbb99b48ec4541594";
     
+    // prevents user from entering in nothing
     if(cityName != "") {
+
+        // begin promise chain
         fetch(apiUrl)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
+            // using the geo location API to set lat and lon of a specific location
             let oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=7347a58ac895179cbb99b48ec4541594";
 
             return fetch(oneCallApi)
@@ -28,11 +40,14 @@ function formSubmitHandeler(event) {
             return response.json();
         })
         .then(function(data) {
+            // clear HTML for new search
             currentWeatherEl.innerHTML = "";
             forecastEl.innerHTML = "";
 
+            // populating main weather dashboard
             currentWeatherEl.appendChild(createCurrentWeather(data, cityName));
 
+            // creating cards and populating forcast
             for(let i = 1; i < 6; i++) {
                 let card = createCard(data, i);
 
@@ -43,23 +58,29 @@ function formSubmitHandeler(event) {
             alert("Not a valid city.");
         });
 
+        // add city name to history
         addToHistory(cityName);
     }
-    document.getElementById("form").reset();
 
+    // reset input tag 
+    document.getElementById("form").reset();
     event.preventDefault();
 };
 
+// function for clicking previously searched cities
 function historyButtonHandeler(event) {
     if(event.target.type === "button") {
+        // retrieving city name from button and setting geo loctaion api url
         let cityName = event.target.textContent;
         let apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=7347a58ac895179cbb99b48ec4541594";
     
+        // begin fetch chain
         fetch(apiUrl)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
+            // using the geo location API to set lat and lon of a specific location
             let oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=7347a58ac895179cbb99b48ec4541594";
 
             return fetch(oneCallApi)
@@ -68,11 +89,14 @@ function historyButtonHandeler(event) {
             return response.json();
         })
         .then(function(data) {
+            // clear HTML for new search
             currentWeatherEl.innerHTML = "";
             forecastEl.innerHTML = "";
 
+            // populating main weather dashboard
             currentWeatherEl.appendChild(createCurrentWeather(data, cityName));
 
+            // creating cards and populating forcast
             for(let i = 1; i < 6; i++) {
                 let card = createCard(data, i);
 
@@ -82,14 +106,17 @@ function historyButtonHandeler(event) {
         .catch(function(error) {
             alert("Not a valid city.");
         });
-    
-        addToHistory(cityName);
     }
 };
 
+/*
+####### Generating HTML Elements #######
+*/
+// function for populating the current weather display
 function createCurrentWeather(weatherObj, cityName) {
     let icon = "https://openweathermap.org/img/w/" + weatherObj.current.weather[0].icon + ".png"
 
+    // creating needed elements
     let weatherDisplayEl = document.createElement("div");
     let h2El = document.createElement("h2");
     let imgEl = document.createElement("img");
@@ -98,6 +125,7 @@ function createCurrentWeather(weatherObj, cityName) {
     let p3El = document.createElement("p");
     let p4El = document.createElement("p");
 
+    // setting data from weatherObj (OpenWeatherMap OneCall v2.5)
     h2El.textContent = cityName + " " + moment().format("M/D/YYYY");
     imgEl.setAttribute("src", icon);
     p1El.textContent = "Temp: " + weatherObj.current.temp + " F";
@@ -112,12 +140,15 @@ function createCurrentWeather(weatherObj, cityName) {
     weatherDisplayEl.appendChild(p3El);
     weatherDisplayEl.appendChild(p4El);
 
+    // returning div element containing children
     return weatherDisplayEl;
 };
 
+// creating card for forecast 
 function createCard(weatherObj, i) {
     let icon = "https://openweathermap.org/img/w/" + weatherObj.daily[i].weather[0].icon + ".png"
 
+    // creating needed elements
     let cardEl = document.createElement("div");
     let cardBodyEl = document.createElement("div");
     let h6El = document.createElement("h6");
@@ -126,9 +157,11 @@ function createCard(weatherObj, i) {
     let p2El = document.createElement("p");
     let p3El = document.createElement("p");
 
+    // setting class list for styling
     cardEl.classList = "col-lg-2 col-md-10 border border-dark card";
     cardBodyEl.classList = "card-body";
 
+    // setting classes for styling and adding data from weatherObj (OpenWeatherMap OneCall v2.5)
     h6El.setAttribute("class", "card-title");
     h6El.textContent = moment().add(i, 'days').format("M/D/YYYY");
 
@@ -152,32 +185,44 @@ function createCard(weatherObj, i) {
 
     cardEl.appendChild(cardBodyEl);
 
+    // returning card div element with apended data
     return cardEl;
 };
 
+/*
+####### History Functions #######
+*/
+// adding and updating historyArray
 function addToHistory(cityName) {
+    // initializing historyArray if user has no previous local storage
     if(historyArray === undefined) {
         historyArray = [];
     }
+
+    // if array is empty push if there are prev entries unshift
     if (historyArray.length === 0) {
         historyArray.push(cityName);
     } else if(!historyArray.includes(cityName)) {
         historyArray.unshift(cityName);
+        // prevents array from getting too long
         if(historyArray.length > 8) {
             historyArray.pop();
         }
     }
     
+    // save to local storage everytime function is called
     localStorage.setItem("History", JSON.stringify(historyArray));
     addHistoryButtons();
 };
 
+// check to see if user has local storage
 function loadHistoryArray() {
     if(localStorage.getItem("History")) {
         historyArray = JSON.parse(localStorage.getItem("History"));
     }
 };
 
+// update page with historyArray
 function addHistoryButtons() {
     if(historyArray !== undefined && historyArray.length > 0) {
         let historyEl = document.querySelector("#history");
@@ -195,6 +240,8 @@ function addHistoryButtons() {
     }
 };
 
-
+/*
+####### Event Listeners #######
+*/
 formEl.addEventListener("submit", formSubmitHandeler);
 historyContainerEl.addEventListener("click", historyButtonHandeler);
